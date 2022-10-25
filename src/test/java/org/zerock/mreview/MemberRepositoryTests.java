992cbd1135e73ee3ee47437a0885746f50f4f873
Ramyunman <prettyshield@naver.com>
2022-10-25 23:40:02 +0900

    SpringBoot Movie Board and Review

---
+++ src/test/java/org/zerock/mreview/MemberRepositoryTests.java
@@ -0,0 +1,52 @@
+package org.zerock.mreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.zerock.mreview.entity.Member;
+import org.zerock.mreview.repository.MemberRepository;
+import org.zerock.mreview.repository.ReviewRepository;
+
+import javax.transaction.Transactional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemberRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    public void insertMembers() {
+
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Member member = Member.builder()
+                    .email("r"+i + "@zerock.org")
+                    .pw("1111")
+                    .nickname("reviewer" + i).build();
+            memberRepository.save(member);
+        });
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMember() {
+
+        Long mid = 1L;  //Member의 mid
+
+        Member member = Member.builder().mid(mid).build();
+
+        //기존
+        //memberRepository.deleteById(mid);
+        //reviewRepository.deleteByMember(member);
+
+        //순서 주의
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(mid);
+    }
+}
